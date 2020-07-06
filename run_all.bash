b5f470bd00154b88bc7886d7e3624dc366cc2062
AviadSar <aaviada@gmail.com>
2020-07-06 03:25:50 +0300

    updates run_all.bash

--- run_all.bash
+++ run_all.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-../random-utils/run_sbatch.py 'EntNet.py --tasks=1 --train --n_tries=2' --gpu=1 --output="./script_results/task_1.out"
+../random-utils/run_sbatch.py 'EntNet.py --tasks=1 --train --n_tries=2' --gpu=1
